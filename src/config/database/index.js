a69bc2697f3c90c3d34becd43e77e8c722472807
mujaddidsi <72315938+mujaddidsi@users.noreply.github.com>
2022-03-28 13:03:25 +0700

    Initial foldering  (#1)
    * Add initial folder
    
    * Add file in every folder initial init

---
+++ src/config/database/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mysql2 = require('mysql2');
+
+const { DB_USER, DB_NAME, DB_PASS } = process.env;
+
+const pool = mysql2.createPool({
+  host: 'localhost',
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+});
+
+module.exports = pool;
