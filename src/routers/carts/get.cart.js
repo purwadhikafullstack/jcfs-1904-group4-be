862a31f6f9064acee93cdfa0bce6ddf3147f4167
jaysonadam <jaysonadam@gmail.com>
2022-04-16 12:29:15 +0700

    Berhasil update cart dari productCard

--- src/routers/carts/get.cart.js
+++ src/routers/carts/get.cart.js
@@ -22,15 +22,17 @@ const getCartById = async (req, res, next) => {
     try {
         const connection = await pool.promise().getConnection();
 
-            const sqlGetUserCartById = `SELECT product_id, product_name, product_price, quantity FROM carts 
+            const sqlGetUserCartById = `SELECT quantity FROM carts 
                                         WHERE user_id = ${req.params.user_id} AND product_id = ${req.params.product_id}`;
 
             const result = await connection.query(sqlGetUserCartById)
             connection.release();
 
             const cart = result[0]
+            const quantity = cart[0]
+            console.log(quantity)
 
-            res.status(200).send({ cart })
+            res.status(200).send({ quantity })
         } catch (error) {
           next (error)
     }
