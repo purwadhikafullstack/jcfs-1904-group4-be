2fc86ef1d7c895689b1be80af5763358c3d748a3
jaysonadam <jaysonadam@gmail.com>
2022-04-19 22:18:35 +0700

    Update delete syntax

--- src/routers/carts/delete.cart.js
+++ src/routers/carts/delete.cart.js
@@ -5,14 +5,14 @@ const deleteFromCart = async (req, res, next) => {
     try {
         const connection = await pool.promise().getConnection();
 
-            const sqlDeleteFromCart = `DELETE FROM cart_details WHERE cart_id = ${req.params.cart_id} AND product_id = ${req.params.product_id}`;
+        const sqlDeleteFromCart = `DELETE FROM cart_details WHERE cart_id = ${req.params.cart_id} AND product_id = ${req.params.product_id}`;
 
-            connection.query(sqlDeleteFromCart)
-            connection.release();
+        connection.query(sqlDeleteFromCart)
+        connection.release();
 
-            res.status(200).send({ message: "Removed from cart" })
-        } catch (error) {
-          next (error)
+        res.status(200).send({ message: "Removed from cart" })
+    } catch (error) {
+        next(error)
     }
 };
 
