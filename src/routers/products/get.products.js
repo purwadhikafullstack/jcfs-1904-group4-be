17a81200ed1a7bd03cb64c0da603a8ec21072074
jaysonadam <jaysonadam@gmail.com>
2022-04-28 21:11:12 +0700

    Pagination, sort, filter sudah mulai jalan

--- src/routers/products/get.products.js
+++ src/routers/products/get.products.js
@@ -1,94 +1,58 @@
 const router = require("express").Router();
 const pool = require("../../config/database");
 
-const getAll = async (req, res, next) => {
-    try {
-        const connection = await pool.promise().getConnection();
-
-        const sqlGetAll = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
-                          JOIN product_categories pc ON p.product_id = pc.product_id 
-                          JOIN categories c ON pc.category_id = c.category_id;`;
-
-        const result = await connection.query(sqlGetAll)
-        connection.release();
-
-        const products = result[0]
-
-        res.status(200).send({ products })
-    } catch (error) {
-      next (error)
-    }
-};
-
-const getSearch = async (req, res, next) => {
-    const filterCategory = req.query.category_name ? ` WHERE category_name = "${req.query.category_name}"` : "";
-
-    const sqlGetSearchProductsCount = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
-                                       JOIN product_categories pc ON p.product_id = pc.product_id 
-                                       JOIN categories c ON pc.category_id = c.category_id
-                                       ${filterCategory} AND product_name LIKE '%${req.query.product_name}%' 
-                                       ORDER BY ${req.query.sortBy} ${req.query.typeSort};`;
-
-    const result = await connection.query(sqlGetSearchProductsCount)
-    connection.release();
-                  
-    const products = result[0]
-                  
-    res.status(200).send({ products });
-};
-
 // Get All Products OR Get Searched Products
 const getAllProducts = async (req, res, next) => {
-    try {
-        const connection = await pool.promise().getConnection();
-        await connection.beginTransaction();
-    
-        try {
-          const connection = await pool.promise().getConnection();
 
-          const sqlGetAllProducts = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
-                                     JOIN product_categories pc ON p.product_id = pc.product_id 
-                                     JOIN categories c ON pc.category_id = c.category_id
-                                     ORDER BY product_name DESC LIMIT ? OFFSET ?;`;
-          const dataGetAllProducts = [ Number(req.query.itemsPerPage), Number(req.query.OFFSET) ]
+      try {
+              const connection = await pool.promise().getConnection();
 
-          // Query base tanpa WHERE category_name
-          // Jika req.query.category_name ada isinya, Query base disisipkan WHERE category_name
-          
-          const filterCategory = req.query.category_name ? ` WHERE category_name = "${req.query.category_name}"` : "";
+              const sqlCountAllProducts = `SELECT COUNT(*) AS count FROM products WHERE is_deleted = 0;`;
 
-          const sqlGetSearchProducts = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
-                                        JOIN product_categories pc ON p.product_id = pc.product_id 
-                                        JOIN categories c ON pc.category_id = c.category_id
-                                        ${filterCategory} AND product_name LIKE '%${req.query.product_name}%' 
-                                        ORDER BY ${req.query.sortBy} ${req.query.typeSort} LIMIT ? OFFSET ?;`
-          const dataGetSearchProducts = [ Number(req.query.itemsPerPage), Number(req.query.OFFSET) ]
+              const sqlGetAllProducts = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
+                                         JOIN product_categories pc ON p.product_id = pc.product_id 
+                                         JOIN categories c ON pc.category_id = c.category_id
+                                         WHERE is_deleted = 0
+                                         ORDER BY product_name DESC LIMIT ? OFFSET ?;`;
+              const dataGetAllProducts = [ Number(req.query.itemsPerPage), Number(req.query.OFFSET) ]
 
-          if (req.query.category_name || req.query.product_name || req.query.sortBy || req.query.typeSort) {
+              // Query base tanpa WHERE category_name
+              // Jika req.query.category_name ada isinya, Query base disisipkan WHERE category_name
+              
+              const filterCategory = req.query.category_id ? ` WHERE category_id = "${req.query.category_id}"` : "";
 
-              const result = await connection.query(sqlGetSearchProducts, dataGetSearchProducts)
+              const sqlCountSearchProducts = `SELECT COUNT(*) AS count FROM product_categories pc
+                                              JOIN products p ON p.product_id = pc.product_id 
+                                              ${filterCategory} AND is_deleted = 0
+                                              AND product_name LIKE '%${req.query.product_name}%';`;
+
+              const sqlGetSearchProducts = `SELECT product_name, p.product_id, product_desc, product_image_name, price, c.category_id, category_name FROM products p 
+                                            JOIN product_categories pc ON p.product_id = pc.product_id 
+                                            JOIN categories c ON pc.category_id = c.category_id
+                                            ${filterCategory} AND product_name LIKE '%${req.query.product_name}%' AND is_deleted = 0 
+                                            ORDER BY ${req.query.sortBy} ${req.query.typeSort} LIMIT ? OFFSET ?;`
+              const dataGetSearchProducts = [ Number(req.query.itemsPerPage), Number(req.query.OFFSET) ]
+
+          if (req.query.category_id || req.query.product_name || req.query.sortBy || req.query.typeSort) {
+
+              const [result] = await connection.query(sqlGetSearchProducts, dataGetSearchProducts)
+              const [count] = await connection.query(sqlCountSearchProducts)
               connection.release();
 
-              const products = result[0]
-
-              res.status(200).send({ products });
+              res.status(200).send({ result, count });
 
           } else {
 
-              const result = await connection.query(sqlGetAllProducts, dataGetAllProducts)
+              const [result] = await connection.query(sqlGetAllProducts, dataGetAllProducts)
+              const [count] = await connection.query(sqlCountAllProducts)
               connection.release();
 
-              const products = result[0];
-
-              res.status(200).send({ products });
+              res.status(200).send({ result, count });
 
           }
-      } catch (error) {
-        next (error)
-      }
-  } catch (error) {
-    next (error)
-  };
+    } catch (error) {
+      next (error)
+    }
 };
 
 // Get Products by ID
@@ -110,8 +74,6 @@ const getProductsById = async (req, res, next) => {
   }
 };
 
-router.get("/all", getAll);
-router.get("/search", getSearch);
 router.get("/get", getAllProducts);
 router.get("/:product_id", getProductsById);
 
--- src/routers/products/index.js
+++ src/routers/products/index.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 
-const getAllRouter = require("./get.products");
-const getCountSearchRouter = require("./get.products");
 const getAllProductsRouter = require("./get.products");
 const getProductsByIdRouter = require("./get.products");
 
-router.use(getAllRouter);
-router.use(getCountSearchRouter);
 router.use(getAllProductsRouter);
 router.use(getProductsByIdRouter);
 
