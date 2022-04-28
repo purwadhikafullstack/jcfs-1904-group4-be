7317d6e4b1290b9d32a88aa7f98ceab40c534be0
jaysonadam <jaysonadam@gmail.com>
2022-04-28 22:13:46 +0700

    Remove product from website applied

--- src/routers/products/index.js
+++ src/routers/products/index.js
@@ -5,9 +5,13 @@ const getProductsByIdRouter = require("./get.products");
 
 const deleteProductRouter = require("./delete.products");
 
+const removeProductRouter = require("./put.products")
+
 router.use(getAllProductsRouter);
 router.use(getProductsByIdRouter);
 
 router.use(deleteProductRouter);
+
+router.use(removeProductRouter);
 
 module.exports = router;
--- src/routers/products/put.products.js
+++ src/routers/products/put.products.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const pool = require("../../config/database");
 
-const deleteProduct = async (req, res, next) => {
+const removeProduct = async (req, res, next) => {
     try {
       const connection = await pool.promise().getConnection();
   
-      const sqlDeleteProducts = `DELETE FROM products WHERE product_id = ${req.params.product_id};`;
+      const sqlRemoveProducts = `UPDATE products SET is_deleted = 1 WHERE product_id = ${req.params.product_id};`;
   
-      connection.query(sqlDeleteProducts);
+      connection.query(sqlRemoveProducts);
       connection.release();
       
       res.status(200).send("Product has been deleted")
@@ -16,6 +16,6 @@ const deleteProduct = async (req, res, next) => {
     }
   };
 
-router.delete("/delete", deleteProduct)
+router.put("/remove", removeProduct)
 
 module.exports = router;
