6cf79120889ee319a852ae0fb8223041a4e511de
jaysonadam <jaysonadam@gmail.com>
2022-05-04 23:39:45 +0700

    Super admin transactions mulai

--- src/routers/transactions/get.transactions.js
+++ src/routers/transactions/get.transactions.js
@@ -1,6 +1,84 @@
 const router = require("express").Router();
 const pool = require("../../config/database");
 
+const getAllTransactions = async (req, res, next) => {
+  try {
+      const connection = await pool.promise().getConnection();
+  
+      const sqlGetAllTransactions = `SELECT * FROM transactions ORDER BY created_at DESC;`;
+  
+      const result = await connection.query(sqlGetAllTransactions);
+      connection.release();
+
+      const transactions = result[0]
+      
+      res.status(200).send({ transactions })
+    } catch (error) {
+      next(error);
+    }
+};
+
+const getPastTransactions = async (req, res, next) => {
+  try {
+      const connection = await pool.promise().getConnection();
+  
+      const sqlGetPastTransactions = `SELECT * FROM transactions WHERE 
+                                      status = 'arrived'
+                                      ORDER BY created_at DESC`;
+
+      const result = await connection.query(sqlGetPastTransactions);
+      connection.release();
+
+      const transactions = result[0]
+      
+      res.status(200).send({ transactions })
+    } catch (error) {
+      next(error);
+    }
+};
+
+const getOngoingTransactions = async (req, res, next) => {
+  try {
+      const connection = await pool.promise().getConnection();
+  
+      const sqlGetOngoingTransactions = `SELECT * FROM transactions WHERE 
+                                         status = 'waiting_payment' 
+                                         OR status = 'waiting_confirmation' 
+                                         OR status = 'payment_accepted' 
+                                         OR status = 'payment_rejected'
+                                         OR status = 'shipped' 
+                                         ORDER BY created_at DESC`;
+
+      const result = await connection.query(sqlGetOngoingTransactions);
+      connection.release();
+
+      const transactions = result[0]
+      
+      res.status(200).send({ transactions })
+    } catch (error) {
+      next(error);
+    }
+};
+
+const getSearchTransactions = async (req, res, next) => {
+  try {
+      const connection = await pool.promise().getConnection();
+  
+      const sqlGetSearchTransaction = `SELECT * FROM transactions WHERE 
+                                       recipient LIKE '%${req.query.recipient_name}%' 
+                                       ORDER BY created_at DESC;`;
+  
+      const result = await connection.query(sqlGetSearchTransaction);
+      connection.release();
+
+      const transactions = result[0]
+      
+      res.status(200).send({ transactions })
+    } catch (error) {
+      next(error);
+    }
+};
+
 const getAllTransactionsById = async (req, res, next) => {
     try {
         const connection = await pool.promise().getConnection();
@@ -132,7 +210,7 @@ const getPastTransactionsByWarehouse = async (req, res, next) => {
     }
 };
 
-const getSearchTransactions = async (req, res, next) => {
+const getSearchTransactionsWh = async (req, res, next) => {
   try {
       const connection = await pool.promise().getConnection();
   
@@ -151,11 +229,18 @@ const getSearchTransactions = async (req, res, next) => {
     }
 };
 
+router.get('/all', getAllTransactions)
+router.get('/past', getPastTransactions)
+router.get('/ongoing', getOngoingTransactions)
 router.get('/search', getSearchTransactions)
+
 router.get('/get/:user_id', getAllTransactionsById)
 router.get('/get/past/:user_id', getPastTransactionsById)
 router.get('/get/ongoing/:user_id', getOngoingTransactionsById)
+
 router.get('/get/details/:transaction_id', getTransactionDetails)
+
+router.get('/wh/search', getSearchTransactionsWh)
 router.get('/wh/all/:warehouse_id', getAllTransactionsByWarehouse)
 router.get('/wh/past/:warehouse_id', getPastTransactionsByWarehouse)
 router.get('/wh/ongoing/:warehouse_id', getOngoingTransactionsByWarehouse)
--- src/routers/transactions/index.js
+++ src/routers/transactions/index.js
@@ -5,11 +5,18 @@ const postTransactionPhotoRouter = require('./post.transactions')
 const postTransactionRouter = require('./post.transactions')
 const postProofRouter = require('./post.transactions')
 
+const getAllTransactionsRouter = require('./get.transactions')
 const getPastTransactionsRouter = require('./get.transactions')
-const getSearchTransactionsRouter =require('./get.transactions')
-const getTransactionDetailsRouter = require('./get.transactions')
-const getAllTransactionsByIdRouter = require('./get.transactions')
 const getOngoingTransactionsRouter = require('./get.transactions')
+const getSearchTransactionsRouter =require('./get.transactions')
+
+const getPastTransactionsByIdRouter = require('./get.transactions')
+const getAllTransactionsByIdRouter = require('./get.transactions')
+const getOngoingTransactionsByIdRouter = require('./get.transactions')
+
+const getTransactionDetailsRouter = require('./get.transactions')
+
+const getSearchTransactionsWhRouter =require('./get.transactions')
 const getAllTransactionsByWarehouseRouter = require('./get.transactions')
 const getPastTransactionsByWarehouseRouter = require('./get.transactions')
 const getOngoingTransactionsByWarehouseRouter = require('./get.transactions')
@@ -21,11 +28,18 @@ router.use(postTransactionPhotoRouter);
 router.use(postTransactionRouter);
 router.use(postProofRouter);
 
+router.use(getAllTransactionsRouter);
 router.use(getPastTransactionsRouter);
-router.use(getSearchTransactionsRouter);
-router.use(getTransactionDetailsRouter);
-router.use(getAllTransactionsByIdRouter);
 router.use(getOngoingTransactionsRouter);
+router.use(getSearchTransactionsRouter);
+
+router.use(getAllTransactionsByIdRouter);
+router.use(getPastTransactionsByIdRouter);
+router.use(getOngoingTransactionsByIdRouter);
+
+router.use(getTransactionDetailsRouter);
+
+router.use(getSearchTransactionsWhRouter);
 router.use(getAllTransactionsByWarehouseRouter);
 router.use(getPastTransactionsByWarehouseRouter);
 router.use(getOngoingTransactionsByWarehouseRouter);
