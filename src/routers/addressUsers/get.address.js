c5c116febdc501cea669d823dc3148cdf9c23017
jaysonadam <jaysonadam@gmail.com>
2022-05-07 18:45:33 +0700

    Get transaction address

--- src/routers/addressUsers/get.address.js
+++ src/routers/addressUsers/get.address.js
@@ -54,8 +54,28 @@ const getChosenAddress = async (req, res, next) => {
   }
 };
 
+const getTransactionAddress = async (req, res, next) => {
+  try {
+    const connection = await pool.promise().getConnection();
+
+    const sqlGetTransactionAddress = `SELECT a.address_id, province, city, village, district, postal_code, detail_address FROM address_users a
+                                      JOIN transactions t ON a.address_id = t.address_id
+                                      WHERE transaction_id = ${req.params.transaction_id};`;
+
+    const result = await connection.query(sqlGetTransactionAddress);
+    connection.release();
+
+    const address = result[0]
+    
+    res.status(200).send({ address })
+  } catch (error) {
+    next(error);
+  }
+};
+
 router.get('/:user_id', getUserAddress);
 router.get('/default/:user_id', getDefaultAddress);
 router.get('/chosen/:address_id', getChosenAddress);
+router.get('/transaction/:transaction_id', getTransactionAddress);
 
 module.exports = router;
--- src/routers/addressUsers/index.js
+++ src/routers/addressUsers/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const getUserAddressRouter = require('./get.address');
 const getDefaultAddressRouter = require('./get.address'); 
 const getChosenAddressRouter = require('./get.address');
+const getTransactionAddressRouter = require('./get.address');
 
 const postUserAddressRouter = require('./post.address');
 
@@ -12,6 +13,7 @@ const putUserAddressRouter = require('./put.address');
 router.use(getUserAddressRouter);
 router.use(getChosenAddressRouter);
 router.use(getDefaultAddressRouter);
+router.use(getTransactionAddressRouter);
 
 router.use(postUserAddressRouter);
 
